bc430e5e6f21805a55dc84c5d5d315bd88c4d1ad
champtiravat <tiravat2016@gmail.com>
2020-02-21 15:44:02 +0700

    :sparkles: Add docs about variables

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
-	fmt.Println("Hello, Bitches!")
+	// Syntax
+	// var varName type = "value"
+	// var numA rune = 10 // 16bit / char = 8bit/1 byte
+	// var numB int8 = 10
+	// var numC byte = 10 // 16bit
+	// var numD int16 = 10
+	// var numE int = 10 // 32bit
+	// var numF int32 = 10
+	// var numG int64 = 10
+	// var numA int = 30 // int32
+	// var a float32 = 10 //
+	// a = 3.141
+	// fmt.Printf("%t\n", a)
+	// var isAdmin string = fmt.Sprintf("hello = [%d]", 10)
+
 }
